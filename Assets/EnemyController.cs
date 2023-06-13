b9a8aa929ec767d92647ce29b45e7bf7a3c51227
tokisakihono <129827583+tokisakihono@users.noreply.github.com>
2023-06-13 14:44:33 +0900

    ゴーレムが歩きません
    ゴーレムが歩きません

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//NavMesh
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    //地点（親オブジェクト）
+    public GameObject Navi;
+
+    //地点カウンター
+    private int counter = 0;
+
+    //オブジェクト数
+    private int number = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //アニメーション
+        GetComponent<Animator>().SetBool("Walk", true);
+
+        //子オブジェクトの数
+        number = Navi.GetComponentInChildren<Transform>().childCount;
+
+        //最初の目標地点 ※無しチェック
+        if (number > 0)
+        {
+            GetComponent<NavMeshAgent>().destination = Navi.transform.GetChild(0).transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //目標地点まで行った？
+        if (GetComponent<NavMeshAgent>().remainingDistance == 0f)
+        {
+            //次の地点
+            counter++;
+
+            //次の目標地点 ※無しチェック
+            if (number > 0)
+            {
+                //次の目標地点へ
+                GetComponent<NavMeshAgent>().destination = Navi.transform.GetChild(counter % number).transform.position;
+            }
+        }
+    }
+}
